d95fa78cfd55ed1f71471a1a163cd391b76c8756
MoonAndEye <atimis19@gmail.com>
2015-06-18 23:53:54 +0900

    before sleep

--- daytrade.py
+++ daytrade.py
@@ -15,14 +15,18 @@ for name in os.listdir('C:/1save/jpStock/raw'):
     history_list.append(name)
     
 history_number = int(len(history_list))
-d0 = history_number -1 #要射的飛標只要這個
-d1 = history_number -2
-d2 = history_number -3
-d3 = history_number -4
-d4 = history_number -5
+
+for i in range(5):
+    'd'+i
+
+#d0 = history_number -1 #要射的飛標只要這個
+#d1 = history_number -2
+#d2 = history_number -3
+#d3 = history_number -4
+#d4 = history_number -5
 
 date_d0 = history_list[d0]  #這是為了給後面的csv檔有日期
 date_d1 = history_list[d1]
 print (date_d0)
-print (date_d1)
+#print (date_d1)
 
