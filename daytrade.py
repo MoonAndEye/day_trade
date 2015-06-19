0d8915ab65ceeba408feebf853815c73a26af984
MoonAndEye <MoonAndEye@users.noreply.github.com>
2015-06-19 09:31:33 +0900

    做出 history_array list

--- daytrade.py
+++ daytrade.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun 18 23:35:05 2015
-
 @author: Moon
 """
 
@@ -13,11 +12,15 @@ import matplotlib.pyplot as plt
 history_list = []
 for name in os.listdir('C:/1save/jpStock/raw'):
     history_list.append(name)
-    
-history_number = int(len(history_list))
 
-for i in range(5):
-    'd'+i
+#可能要從這邊建立,比較符合python的語法
+history_number = int(len(history_list)) - 1
+string = {}
+for i in range(0, 9):
+  string['d' + str(i)] = history_list[history_number]
+  history_number = history_number - 1
+  
+print (string)
 
 #d0 = history_number -1 #要射的飛標只要這個
 #d1 = history_number -2
@@ -25,8 +28,7 @@ for i in range(5):
 #d3 = history_number -4
 #d4 = history_number -5
 
-date_d0 = history_list[d0]  #這是為了給後面的csv檔有日期
-date_d1 = history_list[d1]
-print (date_d0)
+#date_d0 = history_list[d0]  #這是為了給後面的csv檔有日期
+#date_d1 = history_list[d1]
+#print (date_d0)
 #print (date_d1)
-
