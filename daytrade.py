5cc7167622a53baedbf7de7989209bfb7fa7b2a4
MoonAndEye <atimis19@gmail.com>
2015-06-19 20:49:27 +0900

    把file_path加入，這個路徑都是放歷史資料的

--- daytrade.py
+++ daytrade.py
@@ -10,18 +10,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
+file_path = 'C:/1save/jpStock/raw' #檔案路徑的代號，這邊放歷史資料
 history_list = []
-for name in os.listdir('C:/1save/jpStock/raw'):
+for name in os.listdir(file_path):
     history_list.append(name)
 
 #可能要從這邊建立,比較符合python的語法
 #把他寫成 def
+"""
 history_number = int(len(history_list)) - 1
 string = {}
 for i in range(0, 9):
   string['d' + str(i)] = history_list[history_number]
   history_number = history_number - 1
-
+"""
 """
 #下面這一行,是用來排序array 平常不需要用
 b = OrderedDict(sorted(string.items()))
@@ -31,9 +33,19 @@ print (string[key])
 """
 
 
-def sortDataFromNew (source_list):
-    history_number = int(len(source_list)) - 1
+def sortDataFromNew (source_list, index):
+    #source_list,是目標的list,第二個是存放目標的diction名字,第三個是裡面的code
+    history_number = int(len(source_list)) - 1   
+    code = str(index)
+    def_diction = {}
+    for i in range(history_number):
+        def_diction[code + str(i)] = source_list[history_number]
+        history_number = history_number - 1
+    return def_diction
 
+#d0 最新日期
+string = sortDataFromNew (history_list, 'd')
+print (string['d0'])
 
 #d0 = history_number -1 #要射的飛標只要這個
 #d1 = history_number -2
